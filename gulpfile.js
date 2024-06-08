564e1f3bc496d9c9e88e296435f8a17a49279480
Ildar <iledar@icloud.com>
2024-06-08 12:05:38 +0500

    Добавляет шаблонизатор Nunjucks

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,10 @@ const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
 const cleanCSS = require('gulp-clean-css');
 const htmlmin = require('gulp-htmlmin');
+const nunjucksRender = require('gulp-nunjucks-render');
+const data = require('gulp-data');
 const watch = require('gulp-watch');
+const path = require('path');
 
 // Компиляция SCSS в CSS
 gulp.task('sass', function() {
@@ -11,30 +14,64 @@ gulp.task('sass', function() {
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('styles'));
+    .pipe(gulp.dest('dist/styles')); // Сохраняем в папку dist/styles
 });
 
 // Минификация CSS
 gulp.task('minify-css', function() {
-  return gulp.src('styles/styles.css')
+  return gulp.src('dist/styles/styles.css')
     .pipe(cleanCSS({ compatibility: 'ie8' }))
     .pipe(gulp.dest('dist/styles'));
 });
 
 // Минификация HTML
 gulp.task('minify-html', function() {
-  return gulp.src('*.html')
+  return gulp.src('dist/*.html')
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest('dist'));
 });
 
-// Наблюдение за изменениями в SCSS файлах
+// Обработка шаблонов Nunjucks
+gulp.task('nunjucks', function() {
+  return gulp.src('src/pages/**/*.njk')
+    .pipe(data(function(file) {
+      return require('./src/data.json');
+    }))
+    .pipe(nunjucksRender({
+      path: ['src/templates']
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+// Копирование изображений
+gulp.task('images', function() {
+  return gulp.src('src/images/**/*')
+    .pipe(gulp.dest('dist/images'));
+});
+
+// Копирование шрифтов
+gulp.task('fonts', function() {
+  return gulp.src('src/fonts/**/*')
+    .pipe(gulp.dest('dist/fonts'));
+});
+
+// Копирование скриптов
+gulp.task('scripts', function() {
+  return gulp.src('src/scripts/**/*')
+    .pipe(gulp.dest('dist/scripts'));
+});
+
+// Наблюдение за изменениями в SCSS и Nunjucks файлах
 gulp.task('watch', function() {
   gulp.watch('scss/**/*.scss', gulp.series('sass'));
+  gulp.watch('src/**/*.njk', gulp.series('nunjucks'));
+  gulp.watch('src/images/**/*', gulp.series('images'));
+  gulp.watch('src/fonts/**/*', gulp.series('fonts'));
+  gulp.watch('src/scripts/**/*', gulp.series('scripts'));
 });
 
 // Задача по умолчанию
-gulp.task('default', gulp.series('sass', 'watch'));
+gulp.task('default', gulp.series('sass', 'nunjucks', 'images', 'fonts', 'scripts', 'watch'));
 
 // Задача для сборки проекта
-gulp.task('build', gulp.series('sass', 'minify-css', 'minify-html'));
+gulp.task('build', gulp.series('sass', 'minify-css', 'nunjucks', 'minify-html', 'images', 'fonts', 'scripts'));
